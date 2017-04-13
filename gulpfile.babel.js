9bd239589302e10a2c0fc0b670f525fa7c9dcd11
worldofth <worldofth@hotmail.co.uk>
2017-04-13 15:26:46 +0100

    basic layout for main content done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,6 @@
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import gutil from 'gulp-util';
 import sourcemaps from 'gulp-sourcemaps';
 import rename from 'gulp-rename';
 import browserSyncObj from 'browser-sync';
@@ -9,6 +11,7 @@ import musatche from 'gulp-mustache';
 import {Server as karmaServer} from 'karma';
 import {resolve as resolvePath} from 'path';
 import {parseConfig as karmaParseConfig} from 'karma/lib/config';
+import chalk from 'chalk';
 
 import dailyConfig from './daily-config';
 
@@ -16,16 +19,33 @@ const browserSync = browserSyncObj.create(),
 	reload = done => (browserSync.reload(), done && done()),
 	baseDir = dailyConfig.currentProject;
 
+function map_error(err){
+	if(err.fileName){
+		gutil.log(chalk.red(err.name)
+		+ ': '
+		+ chalk.yellow(err.fileName.replace(__dirname, ''))
+		+ ': '
+		+ 'Line '
+		+ chalk.magenta(err.lineNumber)
+		+ ' & Column'
+		+ chalk.magenta(err.columnNumber || err.column)
+		+ ': '
+		+ chalk.blue(err.description));
+	}else{
+		gutil.log(chalk.red(err.name) + ': ' + chalk.yellow(err.message));
+	}
+}
 
 // ====================
 // css - sass
 // ====================
 function buildSass(){
 	return gulp.src(baseDir + '/css/main.scss')
+		.pipe(plumber({errorHandler: map_error}))
 		.pipe(sourcemaps.init())
 		.pipe(sass({
 			//outputStyle: 'compressed'
-		}).on('error', console.log))
+		}))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(baseDir + '/css'))
 		.pipe(browserSync.stream({match: '**/*.css'}));
@@ -47,6 +67,7 @@ exports['watch:sass'] = watchSass;
 // ====================
 function buildJS(){
 	return gulp.src(baseDir + '/js/src/index.js')
+		.pipe(plumber({errorHandler: map_error}))
 		.pipe(gulpWebpack(require('./webpack.config.js'), webpack))
 		.pipe(gulp.dest(baseDir + '/js/dist/'));
 }
@@ -103,6 +124,7 @@ exports['watch:test'] = watchKarmaTestJS;
 // ====================
 function buildHtml(){
 	return gulp.src(baseDir + '/html/index.mustache')
+		.pipe(plumber({errorHandler: map_error}))
 		.pipe(musatche(baseDir + '/html/data.json'))
 		.pipe(rename('index.html'))
 		.pipe(gulp.dest(baseDir + '/'));
