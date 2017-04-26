0d71fa637da24875c573f5c65ebdd31efc9954e8
worldofth <worldofth@hotmail.co.uk>
2017-04-26 14:13:00 +0100

    playing around with applicatives

--- project-4/js/src/index.js
+++ project-4/js/src/index.js
@@ -1,13 +1,18 @@
 //import {seachAndGetVideoItems} from './videoSearch';
 import {log} from './util';
-import {task} from 'folktale/data/task';
+import {of, task} from 'folktale/data/task';
+import Maybe from 'folktale/data/maybe';
 import flip from 'ramda/src/flip';
 import curry from 'ramda/src/curry';
 import Rx from 'rx';
+import compose from 'ramda/src/compose';
+import chain from 'ramda/src/chain';
+import map from 'ramda/src/chain';
+import apply from 'ramda/src/apply';
 
 // getDom: String -> Task(error, DOM)
 var getDom = function(selector){
-	return task(resolver => {
+	return new task(resolver => {
 		var dom = document.querySelector(selector);
 		if(dom){
 			resolver.resolve(dom);
@@ -25,8 +30,17 @@ var subscribe = curry(function(res, rej, strm){
 	strm.subscribe(res, rej);
 });
 
+var combineDom = curry((a, b) => [a, b]);
+var liftA2 = curry((f, fn1, fn2) => fn1.map(f).apply(fn2));
+var getInputOutput = getDom('body').chain(input => getDom('.js-input').chain(output => of({input, output})));
+
+var test = liftA2(combineDom, getDom('body'), getDom('.js-input'));
+//compose(map(log), chain(combineDom('test')), of);
+
 function main(){
-	getDom('.js-input').map(keypressStream).map(subscribe(log, log)).run().future();
+	test.map(log).run().future();
+	//getInputOutput.map(log).run().future();
+	//getDom('.js-input').map(keypressStream).map(subscribe(log, log)).run().future();
 	//seachAndGetVideoItems('extra credits').map(log).run().promise().catch(log);
 }
 
